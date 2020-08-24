3d68c689927fbef687ee989b5e088396ec4bfc21
Pierre-Nicolas Perrin <pnp@jgp.net>
2020-08-24 17:54:16 -0400

    initial commit/var

---
+++ var/var.js
@@ -0,0 +1,14 @@
+
+/*this project is not meant to teach me programming
+but rather the javascript syntax to later develop a
+React frontend*/
+
+let firstName = "Dylan";
+firstName = "oopsies that isn't a first name!"
+
+let lastName = "hehe"
+let fullName = firstName + " " + lastName;
+let age = 18;
+let isEmployed = false;
+
+console.log(fullName);
